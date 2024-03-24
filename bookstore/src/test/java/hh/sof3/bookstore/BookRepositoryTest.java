020153c1c032d18835638f198423a6e210dbf81d
Textiili <erkki.tiili@gmail.com>
2024-03-24 19:19:08 +0200

    deleted meaningless imports

--- bookstore/src/test/java/hh/sof3/bookstore/BookRepositoryTest.java
+++ bookstore/src/test/java/hh/sof3/bookstore/BookRepositoryTest.java
@@ -4,17 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import hh.sof3.bookstore.domain.Book;
 import hh.sof3.bookstore.domain.BookRepository;
 import hh.sof3.bookstore.domain.Category;
 import hh.sof3.bookstore.domain.CategoryRepository;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class BookRepositoryTest {
 
--- bookstore/src/test/java/hh/sof3/bookstore/CategoryRepositoryTest.java
+++ bookstore/src/test/java/hh/sof3/bookstore/CategoryRepositoryTest.java
@@ -4,15 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import hh.sof3.bookstore.domain.Category;
 import hh.sof3.bookstore.domain.CategoryRepository;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class CategoryRepositoryTest {
 
